593149cfc7107bce1bb0806ebb5373dad7895df7
Wendy Quarteer <wendyquarteer@outlook.com>
2022-05-28 13:50:54 +0200

    css style changes+ git status

--- script.js
+++ script.js
@@ -1,177 +1,89 @@
-//add eventListener for when user clicks the button.
-document.getElementById('run').addEventListener('click', () => {
-    //get the value from the input-field.
-    let input = document.getElementById('pokemon').value;
-    //console.log(input);
-    //call the function getData which is going to relate on the input-value.
-    getData(input).catch(error => {
-        alert('No such Pokemon found!' + error)
+document.getElementById('run').addEventListener('click', () => { //add eventListener for when user clicks the button.
+    let input = document.getElementById('pokemon').value; //get the value from the input-field.
+    getData(input).catch(error => { //call the function getData which is going to relate on the input-value.
+        alert('No such Pokemon found!')
         console.log('Error Fetching pokemon')
         console.log(error)
     });
 });
-//concatenate the input (=string) with he API url.
-async function getData(string) {
-    //fetch the pokeAPI and await operator for promise.
-    const response1 = await fetch('https://pokeapi.co/api/v2/pokemon/' + string);
-    //await promise to get data1.
-    const data1 = await response1.json();
-    //show data in console.  This only shows an array with the total of all pokemons with all pokemons inside: results.
-    //console.log(data1);
 
-    //get the requested pokemon.
-    //fetch the pokemon-id.
-    const id = data1.id;
-    //console.log(id);
-    //get the pokemon-name.
-    const name = data1.name;
-    //console.log(name);
-    //get the pokemon pictures.
-    const sprites = data1.sprites;
-    //console.log(sprites);
-    //display first image in console
+
+async function getData(string) { //concatenate the input (=string) with he API url.
+    const response1 = await fetch('https://pokeapi.co/api/v2/pokemon/' + string); //fetch the pokeAPI and await operator for promise.
+    const data1 = await response1.json();  //await promise to get data1.
+
+    const id = data1.id; //get the requested pokemon. //fetch the pokemon-id.
+    const name = data1.name; //get the pokemon-name.
+    const sprites = data1.sprites; //get the pokemon pictures.
     const img = sprites.front_default;
-    //console.log(img);
     const evImg = document.createElement('img');
     evImg.src = img;
-    document.getElementById('pokePic').innerHTML = "";
+    document.getElementById('pokePic').innerHTML = "";//make sure the element is empty before adding img.
     document.getElementById('pokePic').appendChild(evImg);
-    //get the array of the moves objects.
-    const moves = data1.moves;
-    //console.log(moves);
-    //display a list from: move, of the array of objects.
-    const pokeMoves = moves.map(({move}) => move);
-    //console.log(pokeMoves);
-    //create a new random array, to display 4 random numbers for the length of pokeMoves
-    let randomArray = Array.from({length: 4}, () => Math.floor(Math.random() * pokeMoves.length));
-    //console.log(randomArray);
-    //link each random number to a random pokeMove.
-    let randPokeMoves = "";
+    const moves = data1.moves; //get the array of the moves objects.
+    const pokeMoves = moves.map(({move}) => move); //display a list from: move, of the array of objects.
+    let randomArray = Array.from({length: 4}, () => Math.floor(Math.random() * pokeMoves.length)); //create a new random array, to display 4 random numbers for the length of pokeMoves
+    let randPokeMoves = ""; //link each random number to a random pokeMove.
     for (let i = 0; i < randomArray.length; i++) {
         randPokeMoves += pokeMoves[randomArray[i]].name + '\r\n';
     }
-    //console.log(randPokeMoves);
 
-    //fetch the pokeApi species.
-    const response2 = await fetch('https://pokeapi.co/api/v2/pokemon-species/' + string);
-    //await promise to get data2.
-    const data2 = await response2.json();
-    //console.log(data2);
+    const response2 = await fetch('https://pokeapi.co/api/v2/pokemon-species/' + string); //fetch the pokeApi species.
+    const data2 = await response2.json(); //await promise to get data2.
 
-    //make variable to get the evolution-chain.
-    const evoChain = data2.evolution_chain;
-    //console.log(evoChain);
-    //make variable to get the object value.
-    const evolution = Object.values(evoChain);
-    //console.log(evolution);
+    const evoChain = data2.evolution_chain; //make variable to get the evolution-chain.
+    const evolution = Object.values(evoChain); //make variable to get the object value.
 
-    //fetch the object evolution.
-    const response3 = await fetch(`${evolution}`);
-    //console.log(response3)
-    //await promise to get data3.
-    const data3 = await response3.json();
-    //console.log(data3);
+    const response3 = await fetch(`${evolution}`); //fetch the object evolution.
+    const data3 = await response3.json(); //await promise to get data3.
 
-    //get the name of evolution 1.
-    const chain = data3.chain;
-    //console.log(chain1);
+    const chain = data3.chain; //get the name of evolution 1.
     const species1 = chain.species;
-    //console.log(species1);
     const pokeEv1Name = species1.name;
-    //console.log(pokeEv1Name);
-    //fetch the pokeAPI and await operator for promise.
-    const response4 = await fetch('https://pokeapi.co/api/v2/pokemon/' + pokeEv1Name);
-    //await promise to get data4.
-    const data4 = await response4.json();
-    //show data in console.  This only shows an array with the total of all pokemons with all pokemons inside: results.
-    //console.log(data4);
+    const response4 = await fetch('https://pokeapi.co/api/v2/pokemon/' + pokeEv1Name); //fetch the pokeAPI and await operator for promise.
+    const data4 = await response4.json(); //await promise to get data4.
     const sprites1 = data4.sprites;
-    //console.log(sprites1);
-    //display image in console
     const img1 = sprites1.front_default;
-    //console.log(img1);
-    //create new img-element.
-    const evImg1 = document.createElement('img');
+    const evImg1 = document.createElement('img'); //create new img-element.
     evImg1.src = img1;
-    //clear the images from last search so they don't add-up.
-    document.getElementById('evoPics').innerHTML = "";
+    document.getElementById('evoPics').innerHTML = ""; //clear the images from last search so they don't add-up.
     document.getElementById('evoPics').appendChild(evImg1);
 
-    if (chain.evolves_to.length !== 0) {
-        //get the name of evolution 2.
+    if (chain.evolves_to.length !== 0) { //get the name of evolution 2.
         const evolvesTo = chain.evolves_to;
-        //console.log(evolvesTo);
         const evolvesTo0 = evolvesTo[0];
-        //console.log(evolvesTo0);
         const species2 = evolvesTo0.species;
-        //console.log(species2);
         const pokeEv2Name = species2.name;
-        //console.log(pokeEv2Name);
-        //fetch the pokeAPI and await operator for promise.
-        const response5 = await fetch('https://pokeapi.co/api/v2/pokemon/' + pokeEv2Name);
-        //await promise to get data5.
-        const data5 = await response5.json();
-        //show data in console.  This only shows an array with the total of all pokemons with all pokemons inside: results.
-        //console.log(data5);
+        const response5 = await fetch('https://pokeapi.co/api/v2/pokemon/' + pokeEv2Name); //fetch the pokeAPI and await operator for promise.
+        const data5 = await response5.json(); //await promise to get data5.
         const sprites2 = data5.sprites;
-        //console.log(sprites2);
-        //display image in console
         const img2 = sprites2.front_default;
-        //console.log(img2);
-        //create new img-element.
-        const evImg2 = document.createElement('img');
+        const evImg2 = document.createElement('img'); //create new img-element.
         evImg2.src = img2;
-        //document.getElementById('evoPics').innerHTML = "";
         document.getElementById('evoPics').appendChild(evImg2);
 
-        if (evolvesTo0.evolves_to.length !== 0) {
-            //get the name of evolution 3.
+        if (evolvesTo0.evolves_to.length !== 0) { //get the name of evolution 3.
             const evolvesToTo = evolvesTo0.evolves_to;
-            //console.log(evolvesToTo);
             const evolvesToTo0 = evolvesToTo[0];
-            //console.log(evolvesToTo0);
             const species3 = evolvesToTo0.species;
-            //console.log(species3);
             const pokeEv3Name = species3.name;
-            //console.log(pokeEv3Name);
-            //fetch the pokeAPI and await operator for promise.
-            const response6 = await fetch('https://pokeapi.co/api/v2/pokemon/' + pokeEv3Name);
-            //await promise to get data6.
-            const data6 = await response6.json();
-            //show data in console.  This only shows an array with the total of all pokemons with all pokemons inside: results.
-            //console.log(data6);
+            const response6 = await fetch('https://pokeapi.co/api/v2/pokemon/' + pokeEv3Name); //fetch the pokeAPI and await operator for promise.
+            const data6 = await response6.json(); //await promise to get data6.
             const sprites3 = data6.sprites;
-            //console.log(sprites3);
-            //display image in console
             const img3 = sprites3.front_default;
-            //console.log(img3);
-            //create new img-element.
-            const evImg3 = document.createElement('img');
+            const evImg3 = document.createElement('img'); //create new img-element.
             evImg3.src = img3;
-            //document.getElementById('evoPics').innerHTML = "";
             document.getElementById('evoPics').appendChild(evImg3);
         }
     }
-    //make variable for the template.
-    const template = document.getElementById('pokeFound');
-    //console.log(template + " template");
-    //make variable for target.
-    const target = document.getElementById('target');
-    //console.log(target + " target");
-    //make variable to clone the template.
-    const clone = template.content.cloneNode(true);
-    //console.log(clone.innerHTML + " clone");
-    //clone.querySelector(".img").src = img;
+    const template = document.getElementById('pokeFound'); //make variable for the template.
+    const target = document.getElementById('target'); //make variable for target.
+    const clone = template.content.cloneNode(true); //make variable to clone the template.
     clone.querySelector(".name").innerText = name;
     clone.querySelector(".id").innerText = id;
     clone.querySelector(".moves").innerText = randPokeMoves;
-    //clone the template and insert it in the ol.
-    //Right before container will be filled, empty container.
-    target.innerHTML = " ";
-    target.appendChild(clone);
-
-
-
+    target.innerHTML = " "; //Right before container will be filled, empty container.
+    target.appendChild(clone); //clone the template and insert it in the ol.
 }
 
 
